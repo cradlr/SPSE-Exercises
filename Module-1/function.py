bf28c4afa3a653d328987cabd8f00edf9d0234cf
cradlr <mohittirkey1992@gmail.com>
2014-05-10 21:11:32 +0530

    Update function.py

--- Module-1/function.py
+++ Module-1/function.py
@@ -5,7 +5,9 @@ import sys
 def print5times(line_to_print):
     for count in range(0,5):
         print line_to_print
-
-
-if len(sys.argv) > 1:
-    print5times(sys.argv[1])
+        
+        
+        
+        
+        
+print5times(sys.argv[1])
